89d613a8b2146e8881f07b05c6ebc3f24ac290eb
Vincent <vincent360@outlook.com>
2021-05-30 17:15:32 +0200

    separated js files

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-// Parallax variables
-let controller = new ScrollMagic.Controller();
-let timeline = new TimelineMax();
-
 // Hamburger menu consts
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
@@ -12,21 +8,3 @@ console.log("Home background thanks to https://unsplash.com/@orwhat");
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
 })
-
-// Animations
-timeline
-    .to('.bg1', 3, {y: -500})
-    .to('.content', 3, {top: '0%'}, '-=3')
-    .fromTo('.my-story-wrapper', {opacity: 0}, {opacity: 1, duration: 1})
-
-// What triggers the animations
-let scene = new ScrollMagic.Scene({
-    triggerElement: 'section',
-    duration: '100%',
-    triggerHook: 0,
-})
-
-// Hooking up animation to the scene
-    .setTween(timeline)
-    .setPin('section')
-    .addTo(controller)
---
+++ scrollmagic.js
@@ -0,0 +1,21 @@
+// Parallax variables
+let controller = new ScrollMagic.Controller();
+let timeline = new TimelineMax();
+
+// Animations
+timeline
+    .to('.bg1', 3, {y: -500})
+    .to('.content', 3, {top: '0%'}, '-=3')
+    .fromTo('.my-story-wrapper', {opacity: 0}, {opacity: 1, duration: 1})
+
+// What triggers the animations
+let scene = new ScrollMagic.Scene({
+    triggerElement: 'section',
+    duration: '100%',
+    triggerHook: 0,
+})
+
+// Hooking up animation to the scene
+    .setTween(timeline)
+    .setPin('section')
+    .addTo(controller)
