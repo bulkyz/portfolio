74b65e0f2993a3ee7437cc722f6d64002f4b42d9
Vincent <vincent360@outlook.com>
2021-05-30 17:06:05 +0200

    finished home page

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ toggleButton.addEventListener('click', () => {
 timeline
     .to('.bg1', 3, {y: -500})
     .to('.content', 3, {top: '0%'}, '-=3')
-    .fromTo('.my-story', {opacity: 0}, {opacity: 1, duration: 1})
+    .fromTo('.my-story-wrapper', {opacity: 0}, {opacity: 1, duration: 1})
 
 // What triggers the animations
 let scene = new ScrollMagic.Scene({
