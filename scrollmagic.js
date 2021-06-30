cd844fddf08b8e9d2904e18ac0d69ad56fc7ed91
Vincent <vincent360@outlook.com>
2021-06-30 23:03:05 +0200

    changed about me layout

--- scrollmagic.js
+++ scrollmagic.js
@@ -7,6 +7,7 @@ timeline
     .to('.bg1', 3, {y: -500})
     .to('.content', 3, {top: '0%'}, '-=3')
     .fromTo('.my-story-wrapper', {opacity: 0}, {opacity: 1, duration: 1})
+    .fromTo('.my-story-button-container', {opacity: 0}, {opacity: 1, duration: 1})
 
 // What triggers the animations
 let scene = new ScrollMagic.Scene({
